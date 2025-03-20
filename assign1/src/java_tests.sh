ab2ad119687391b9b3ad4124777142ab7a62e859
José Diogo Alves Granja <up202205143@edu.fe.up.pt>
2025-03-20 09:48:05 +0000

    java script and test file

--- assign1/src/java_tests.sh
+++ assign1/src/java_tests.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-# Function to run tests
+# Function to run tests and capture execution time
 run_tests() {
     local start=$1
     local end=$2
     local increment=$3
     local version=$4
     local blockSize=$5
+    local baseline_time=0
+    local exec_time=0
 
     # Check if the Java program exists
-    if [ ! -f matrixproduct.class ]; then
-        echo "Compiling matrixproduct.java..."
-        javac matrixproduct.java
+    if [ ! -f matrixproduct_test.class ]; then
+        echo "Compiling matrixproduct_test.java..."
+        javac matrixproduct_test.java
     fi
 
     for ((size=start; size<=end; size+=increment)); do
         for run in {1..3}; do
             if [ -z "$blockSize" ]; then
-                echo "Running version $version with matrix size $size x $size, run $run"
-                java matrixproduct $version $size
+                # Run the Java program and capture the execution time
+                exec_time=$(java matrixproduct_test $version $size | grep 'Time:' | awk '{print $2}')
+                echo "Running version $version with matrix size $size x $size, run $run: $exec_time" >> results-java-$version.txt
+
             else
-                echo "Running version $version with matrix size $size x $size, block size $blockSize, run $run"
-                java matrixproduct $version $size $blockSize
+                # Run the Java program with block size and capture the execution time
+                exec_time=$(java matrixproduct_test $version $size $blockSize | grep 'Time:' | awk '{print $2}')
+                echo "Running version $version with matrix size $size x $size, block size $blockSize, run $run: $exec_time" >> results-java-$version.txt
             fi
         done
     done
@@ -33,10 +38,8 @@ run_tests 600 3000 400 1
 # 2. From 600x600 to 3000x3000 matrix sizes with increments of 400 for the onMultLine() version
 run_tests 600 3000 400 2
 
-# 3. From 4096x4096 to 10240x10240 with intervals of 2048 for the onMultLine() version
-run_tests 4096 10240 2048 2
-
-# 4. For different block sizes for the onMultBlock() version
+# 3. For different block sizes for the onMultBlock() version
 for blockSize in 128 256 512; do
     run_tests 4096 10240 2048 3 $blockSize
 done
+
